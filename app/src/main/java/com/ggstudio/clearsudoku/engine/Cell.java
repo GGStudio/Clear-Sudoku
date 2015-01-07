0dd1979dda3f2d0a84361064e0e7887339211932
jackxlifer <zolan7zolan>
2015-01-07 12:27:33 +0200

    added engine

---
+++ app/src/main/java/com/ggstudio/clearsudoku/engine/Cell.java
@@ -0,0 +1,149 @@
+package com.ggstudio.clearsudoku.engine;
+
+import com.ggstudio.clearsudoku.engine.Enums.*;
+public class Cell {
+    public Cell(int x,int y) {
+        this(x,y,-1);
+    }
+
+    public Cell(int x, int y, int val){
+        this.setX(x);
+        this.setY(y);
+
+        digits = new boolean[9];
+
+        for(int i=0;i<9;i++)
+            digits[i]=false;
+
+        if (val>0 && val<10)
+        {
+            _state = CellState.DEFINED;
+        }
+        else
+        {
+            _state = CellState.BLANK;
+        }
+        _value = val;
+        _count = 0;
+    }
+
+    public void setState(CellState _state) {
+        this._state = _state;
+    }
+
+    public CellState getState() {
+        return _state;
+    }
+
+    private void setX(int x) {
+        this._x = x;
+    }
+
+    private void setY(int y) {
+        this._y = y;
+    }
+
+    public int getX() {
+        return _x;
+    }
+
+    public int getY() {
+        return _y;
+    }
+
+/*	public void exclude(int excludeValue){
+		if (_state==CellState.BLANK){
+
+			if(excludeValue>0 && excludeValue<10){
+
+				if (digits[excludeValue-1]){
+
+					digits[excludeValue-1]=false;
+					_count--;
+					if (_count==1) {
+						_state=CellState.DEFINED;
+					}
+				}
+			}
+		}
+	}
+	*/
+
+    public void trigger(int digit){
+        if(digit<0 || digit>=9) return;
+        if (_state==CellState.START)return;
+
+        digits[digit]=!digits[digit];
+
+        if (digits[digit])
+            _count++;
+        else
+            _count--;
+
+        if (_count==1){
+            _value=findSingleValue();
+            _state=CellState.DEFINED;
+        } else {
+            _value=-1;
+            _state=CellState.BLANK;
+        }
+    }
+
+    private int findSingleValue(){
+        int i;
+        int s=0;
+        int count=0;
+        for(i=0;i<9;i++){
+            if (digits[i]){
+                count++;
+                s=i;
+            }
+        }
+        if(count==1) return s;
+        return -1;
+    }
+
+    public boolean hasDigit(int i){
+        if(i>0 && i<10){
+            return digits[i];
+        } else
+            return false;
+    }
+
+    public boolean[] getDigits() {
+        return digits;
+    }
+
+    public void setRow(Group row) {
+        this.row = row;
+    }
+
+    public void setBlock(Group block) {
+        this.block = block;
+    }
+
+    public void setColumn(Group column) {
+        this.column = column;
+    }
+
+    public void setValue(int value) {
+        this._value = value;
+        _state=CellState.START;
+    }
+    public boolean isDefined(){
+        return (_state==CellState.DEFINED);
+    }
+
+    public int getValue() {
+        return _value;
+    }
+
+    private CellState _state;
+
+    public Group row,block,column;
+
+    private boolean[] digits;
+    private int _value;
+    private int _count;
+    private int _x,_y;
+}
---
+++ app/src/main/java/com/ggstudio/clearsudoku/engine/SudokuField.java
@@ -0,0 +1,104 @@
+package com.ggstudio.clearsudoku.engine;
+
+
+import com.ggstudio.clearsudoku.engine.Enums.*;
+public class SudokuField {
+
+    public SudokuField() {
+        cells	= new Cell[9][9];
+        rows 	= new Group[9];
+        cols	= new Group[9];
+        blocks 	= new Group[9];
+
+        createCells();
+        createRows();
+        createColumns();
+        createGroups();
+    }
+
+
+    private void createCells(){
+        for (int i = 0; i < 9; i++) {
+            for(int j=0;j<9;j++){
+                cells[i][j]=new Cell(i+1, j+1);
+            }
+        }
+    }
+
+    private void createRows(){
+        for(int i=0;i<9;i++)
+            rows[i]=new Group(GroupType.ROW,cells[i][0],cells[i][1],cells[i][2],cells[i][3],cells[i][4],cells[i][5],cells[i][6],cells[i][7],cells[i][8]);
+    }
+
+    private void createColumns(){
+        for(int i=0;i<9;i++)
+            cols[i]= new Group(GroupType.COLUMN,cells[0][i],cells[1][i], cells[2][i], cells[3][i], cells[4][i], cells[5][i], cells[6][i], cells[7][i], cells[8][i]);
+    }
+
+    private void createGroups(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                blocks[i*3+j]=new Group(GroupType.BLOCK,
+                        cells[3*i][3*j],cells[3*i][3*j+1],cells[3*i][3*j+2],
+                        cells[3*i+1][3*j],cells[3*i+1][3*j+1],cells[3*i+1][3*j+2],
+                        cells[3*i+2][3*j],cells[3*i+2][3*j+1],cells[3*i+2][3*j+2]);
+            }
+        }
+    }
+    public void fillStartingValues(){
+        cells[1][2].setValue(5);
+        cells[3][4].setValue(2);
+    }
+
+    public boolean checkDefined(){
+        boolean flag = true;
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if (!cells[i][j].isDefined())
+                    flag =false;
+            }
+        }
+
+        return flag;
+    }
+
+    public boolean checkFinished(){
+        if (!checkDefined()) return false;
+
+        int sum=0;
+        for(int i=0;i<9;i++)
+            sum+=rows[i].getSum();
+        if (sum!=45)
+            return false;
+
+        sum=0;
+        for(int i=0;i<9;i++)
+            sum+=cols[i].getSum();
+        if (sum!=45)
+            return false;
+
+        sum=0;
+        for(int i=0;i<9;i++)
+            sum+=blocks[i].getSum();
+        if (sum!=45)
+            return false;
+
+        for(int i=0;i<9;i++)
+            if (!rows[i].isAllUnique())
+                return false;
+
+        for(int i=0;i<9;i++)
+            if (!cols[i].isAllUnique())
+                return false;
+
+        for(int i=0;i<9;i++)
+            if (!blocks[i].isAllUnique())
+                return false;
+        return true;
+    }
+    public Cell[][] cells;
+    public Group[] rows;
+    public Group[] cols;
+    public Group[] blocks;
+}
